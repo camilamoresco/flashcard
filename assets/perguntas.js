37e105022211cd7feb277f207bd8edcefe066fbc
camilamoresco <165938803+camilamoresco@users.noreply.github.com>
2024-10-26 11:42:24 +0000

    aula 7

---
+++ assets/perguntas.js
@@ -0,0 +1,26 @@
+criaCartao (
+    'Geografia',
+    'Qual a capital da Argentina',
+    'Buenos Aires'
+)
+
+criaCartao (
+    'Esporte',
+    'Em qual cidade foi realizada as olimpíadas de 2012',
+    'Londres'
+)
+criaCartao (
+    'Matemática',
+    'O que é o Teorema de Pitágoras?',
+    'É uma expressão matemática que relaciona as medidas dos lados de um triângulo retângulo, conhecidos como catetos e hipotenusa.'
+)
+criaCartao(
+    'Química',
+    'Qual a famosa frase do químico francês Lavoisier?',
+    'Na natureza nada se cria, nada se perde, tudo se transforma.'
+)
+criaCartao (
+    'Biologia'
+    'O que é Complexo de Golgi'
+    'É uma organela celular que está relacionada com o processo de secreção de substâncias'
+)
